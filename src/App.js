0f7502bfbaf091a0a0c8b6dd1d30d8a7b418c716
chenxing1992 <crimson_sabre@hotmail.com>
2021-05-03 00:58:06 +0800

    added headings 2

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ class App extends Component{
   render(){
   const { monsters, searchField} = this.state;
   const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+   );
   return (
     <div className="App">
     
-
+      <h1> Dua King Kong </h1>
       <SearchBox 
          placeholder='Search Monster'
          handleChange={e => {
